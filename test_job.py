ff0d98d39d432d1578ed02652d90db9cddbe0668
mohit <mp65742@gmail.com>
2026-04-06 08:58:02 +0530

    Removing test files

--- test_job.py
+++
@@ -1,5 +0,0 @@
-import sys
-from backend.analysis_engine import analyze_parallel
-
-file_path = "tmp/2022-03-14-Qakbot-with-Cobalt-Strike-and-VNC-module.pcap"
-analyze_parallel(file_path, _job_id_override="test_job_123")
