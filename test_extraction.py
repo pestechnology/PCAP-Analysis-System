87706b6872b15e89c61741e15d01b8ca903ab98e
mohit <mp65742@gmail.com>
2026-04-06 08:58:02 +0530

    Removing test files

--- test_extraction.py
+++
@@ -1,7 +0,0 @@
-import sys
-from backend.core.credential_extractor import extract_credentials
-from backend.core.file_extractor import extract_files_metadata
-
-pcap_file = "tmp/2022-03-14-Qakbot-with-Cobalt-Strike-and-VNC-module.pcap"
-print("Files:", extract_files_metadata(pcap_file))
-print("Creds:", extract_credentials(pcap_file))
